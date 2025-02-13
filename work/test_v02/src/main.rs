c7cf194c1f8688d444008b9a2fa121f4ffacc068
Mezni <mamezni@gmail.com>
2025-02-13 20:26:20 +0000

    commit 2025/02/13

--- work/test_v02/src/main.rs
+++ work/test_v02/src/main.rs
@@ -3,7 +3,7 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
-    let mut generator = generator::EventGenerator::new(10);
+    let mut generator = generator::EventGenerator::new(1000);
     for _ in 0..10 {
         println!("{:?}", generator.next());
     }
