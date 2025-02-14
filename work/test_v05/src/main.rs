9cf85973cdb539d0be761da8284ab99dea101bdb
Mezni <mamezni@gmail.com>
2025-02-14 20:50:29 +0000

    commit 2025/02/14

--- work/test_v05/src/main.rs
+++ work/test_v05/src/main.rs
@@ -3,12 +3,12 @@ mod event_generator;
 mod event_processor;
 
 //use tokio::sync::mpsc;
-//use event_generator::event_generator;
+use event_generator::EventGenerator;
 //use event_processor::event_processor;
 
 #[tokio::main]
 async fn main() {
-    let mut event_generator = event_generator::EventGenerator::new(100);
+    let mut event_generator = EventGenerator::new(100);
     for _ in 0..5 {
         if let Some(event) = event_generator.next() {
             println!("{}", event);
