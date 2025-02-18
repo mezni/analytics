e8b830c5c437c7ef554e685f272d89f048d2328f
Mezni <mamezni@gmail.com>
2025-02-18 21:12:52 +0000

    commit 2025/02/18

--- work/test_v03/src/main.rs
+++ work/test_v03/src/main.rs
@@ -1,7 +1,9 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 mod models;
+mod config;
 
 use crate::models::Status;
+use dotenv::dotenv;
 
 async fn status() -> impl Responder {
     HttpResponse::Ok().json(Status {
@@ -11,8 +13,12 @@ async fn status() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    dotenv().ok();
+    let config=crate::config::Config::from_env().unwrap();
+
     HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind(("0.0.0.0", 3000))?
+        .bind(format!("{}:{}", config.host, config.port))?
         .run()
         .await
 }
