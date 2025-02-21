c4b1155771236fd47f40b22a3788cfa5ab989a6c
Mezni <mamezni@gmail.com>
2025-02-21 16:43:58 +0000

    commit 2025/02/21

--- work/test_v05/src/main.rs
+++ work/test_v05/src/main.rs
@@ -1,7 +1,12 @@
+// src/main.rs
+mod database;
+mod error;
 mod generator;
 mod processor;
 mod queue;
 
+use database::{get_pool, initialize_database};
+use error::AppError;
 use generator::EventGenerator;
 use processor::EventProcessor;
 use queue::Queue;
@@ -13,8 +18,13 @@ const MAC_COUNT: usize = 10_000;
 const MAC_INV_COUNT: usize = 5;
 
 #[tokio::main]
-async fn main() -> sled::Result<()> {
+async fn main() -> Result<(), AppError> {
     println!("Start");
+
+    let pool = get_pool("macs.db")?;
+
+    initialize_database(&pool)?;
+
     let fields = vec![
         "mac_address".to_string(),
         "event_time".to_string(),
@@ -24,13 +34,12 @@ async fn main() -> sled::Result<()> {
         "port_dst".to_string(),
         "event_type".to_string(),
     ];
-
-    let queue = Arc::new(Queue::new("queue_db")?);
     let event_generator = EventGenerator::new(MAC_COUNT, MAC_INV_COUNT).await;
     let event_processor = Arc::new(Mutex::new(EventProcessor::new(fields)));
+    let queue: Arc<Queue> = Arc::new(Queue::new("queue_db")?);
 
     // Producer Task
-    let producer_queue = Arc::clone(&queue);
+    let producer_queue: Arc<Queue> = Arc::clone(&queue);
     tokio::spawn(async move {
         for event in event_generator {
             if let Err(e) = producer_queue.push(&event).await {
@@ -40,18 +49,24 @@ async fn main() -> sled::Result<()> {
     });
 
     // Consumer Task
-    let consumer_queue = Arc::clone(&queue);
-    let processor = Arc::clone(&event_processor);
+    let consumer_queue: Arc<Queue> = Arc::clone(&queue);
+    let processor: Arc<Mutex<EventProcessor>> = Arc::clone(&event_processor);
     tokio::spawn(async move {
         loop {
-            if let Ok(Some(popped_event)) = consumer_queue.pop().await {
-                let mut processor = processor.lock().await;
-                if let Err(e) = processor.process(popped_event).await {
-                    eprintln!("Error processing event: {}", e);
+            match consumer_queue.pop().await {
+                Ok(Some(popped_event)) => {
+                    let mut processor = processor.lock().await;
+                    if let Err(e) = processor.process(popped_event).await {
+                        eprintln!("Error processing event: {}", e);
+                    }
                 }
-            } else {
-                // No event available, avoid busy-waiting
-                tokio::time::sleep(Duration::from_millis(100)).await;
+                Ok(None) => {
+                    // No event available, avoid busy-waiting
+                    tokio::time::sleep(Duration::from_millis(100)).await;
+                }
+                Err(e) => {
+                    eprintln!("Error popping event: {}", e);
+                }
             }
         }
     });
