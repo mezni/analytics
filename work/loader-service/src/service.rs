3139634d1fe63e801764e74fa40749b267b958a9
Mezni <mamezni@gmail.com>
2025-03-22 12:26:47 -0400

    commit 2025/03/22

--- work/loader-service/src/service.rs
+++ work/loader-service/src/service.rs
@@ -1,6 +1,7 @@
 use crate::db::connection;
 use crate::errors::AppError;
 use crate::repositories::BatchRepository;
+use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio_postgres::Client;
@@ -8,6 +9,7 @@ use tokio_postgres::Client;
 pub struct LoadService {
     client: Arc<Mutex<Client>>,
     batch_repo: BatchRepository,
+    carriers_map: HashMap<String, String>,
 }
 
 impl LoadService {
@@ -19,33 +21,26 @@ impl LoadService {
             client: Arc::clone(&client_arc),
         };
 
-        Ok(LoadService {
-            client: client_arc,
-            batch_repo,
-        })
+        // Initialize an empty carriers_map
+        let carriers_map = HashMap::new();
+
+        Ok(LoadService { client: client_arc, batch_repo, carriers_map })
     }
 
     pub async fn load(&self, file_path: &str) -> Result<(), AppError> {
-        println!("Starting batch for file: {}", file_path);
-
-        // Insert batch and retrieve the batch ID
-        let batch_id = match self.batch_repo.insert_batch(file_path).await {
-            Ok(id) => id,
-            Err(e) => {
-                eprintln!("Failed to insert batch: {}", e);
-                return Err(e);
-            }
-        };
-
+        let batch_id = self.batch_repo.insert_batch(file_path).await?;
         println!("Inserted batch with ID: {}", batch_id);
 
-        // Update the batch to Completed
-        if let Err(e) = self.batch_repo.update_batch(batch_id, "Completed").await {
-            eprintln!("Failed to update batch {}: {}", batch_id, e);
-            return Err(e);
-        }
-
-        println!("Batch {} marked as Completed.", batch_id);
+        self.batch_repo.update_batch(batch_id, "Completed").await?;
+        println!("Batch {} marked as Completed.", batch_id);        
         Ok(())
+    }
+
+    pub fn add_carrier(&mut self, code: String, name: String) {
+        self.carriers_map.insert(code, name);
+    }
+
+    pub fn get_carrier_name(&self, code: &str) -> Option<&String> {
+        self.carriers_map.get(code)
     }
 }
