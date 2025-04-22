c311e67138717f215799ad6bddff781d1e8ddb4b
Mezni <mamezni@gmail.com>
2025-04-22 11:37:11 -0400

    commit 2025/04/22

--- config/init.sql
+++ config/init.sql
@@ -1,5 +1,20 @@
-CREATE TABLE IF NOT EXISTS periods (
-    period_id SERIAL PRIMARY KEY,
+----------------------
+-- Baseline
+----------------------
+CREATE TABLE IF NOT EXISTS gloabl_config (
+    gloabl_config_id SERIAL PRIMARY KEY,
+    key TEXT,
+    value TEXT
+);
+
+CREATE TABLE roam_directions (
+    roam_direction_id SERIAL PRIMARY KEY,
+    direction VARCHAR(3) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS dates (
+    date_id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     year INT NOT NULL,
     quarter INT NOT NULL,
@@ -9,44 +24,27 @@ CREATE TABLE IF NOT EXISTS periods (
     day_name TEXT NOT NULL,
     week_of_year INT NOT NULL,
     is_weekend BOOLEAN NOT NULL,
-    period TEXT
+    date_str TEXT
 );
 
-CREATE TABLE roam_directions (
-    roam_direction_id SERIAL PRIMARY KEY,
-    direction VARCHAR(3) NOT NULL,
-    description TEXT
+CREATE TABLE IF NOT EXISTS countries (
+    country_id SERIAL PRIMARY KEY,
+    iso TEXT,
+    common_name TEXT,
+    name_en TEXT,
+    name_fr TEXT,
+    prefix TEXT,
+    prefix_flag CHAR(1),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by TEXT,
+    updated_at TIMESTAMP,
+    updated_by TEXT
 );
 
 CREATE TABLE metrics_type (
     metric_type_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT
-);
-
-CREATE TABLE metric_definition (
-    metric_definition_id SERIAL PRIMARY KEY,
-    roam_direction_id INTEGER NOT NULL REFERENCES roam_directions(roam_direction_id),
-    metric_type_id INTEGER NOT NULL REFERENCES metrics_type(metric_type_id),  
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    is_active BOOLEAN,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    created_by TEXT,
-    updated_at TIMESTAMP,
-    updated_by TEXT    
-);
-
-CREATE TABLE IF NOT EXISTS countries (
-    country_id SERIAL PRIMARY KEY,
-    iso TEXT,
-    name TEXT,
-    name_fr TEXT,
-    prefix TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    created_by TEXT,
-    updated_at TIMESTAMP,
-    updated_by TEXT
 );
 
 CREATE TABLE IF NOT EXISTS networks (
@@ -77,7 +75,7 @@ CREATE TABLE IF NOT EXISTS operators (
 );
 
 CREATE TABLE IF NOT EXISTS prefixes (
-    prefixe_id SERIAL PRIMARY KEY,
+    prefix_id SERIAL PRIMARY KEY,
     country_id INT,
     operator_id INT,
     cc TEXT,
@@ -89,6 +87,17 @@ CREATE TABLE IF NOT EXISTS prefixes (
     updated_by TEXT
 );
 
+CREATE TABLE IF NOT EXISTS batch_execs (
+    id SERIAL PRIMARY KEY,
+    batch_name TEXT NOT NULL,
+    source_type TEXT,
+    source_name TEXT,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,        
+    corr_id INT,
+    batch_status TEXT
+);
+
 CREATE TABLE subscribers (
     subscriber_id SERIAL PRIMARY KEY,
     imsi VARCHAR(20) NOT NULL,
@@ -98,11 +107,24 @@ CREATE TABLE subscribers (
     last_seen TIMESTAMP    
 );
 
+CREATE TABLE metric_definition (
+    metric_definition_id SERIAL PRIMARY KEY,
+    roam_direction_id INTEGER NOT NULL REFERENCES roam_directions(roam_direction_id),
+    metric_type_id INTEGER NOT NULL REFERENCES metrics_type(metric_type_id),  
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    is_active BOOLEAN,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by TEXT,
+    updated_at TIMESTAMP,
+    updated_by TEXT    
+);
+
 CREATE TABLE metrics (
     metric_id SERIAL PRIMARY KEY,
     metric_definition_id INTEGER NOT NULL REFERENCES metric_definition(metric_definition_id),
     batch_id INTEGER,
-    period_id INTEGER NOT NULL REFERENCES periods(period_id),
+    date_id INTEGER NOT NULL REFERENCES dates(date_id),
     country_id INTEGER REFERENCES countries(country_id),
     operator_id INTEGER REFERENCES operators(operator_id),  
     subscriber_id INTEGER REFERENCES subscribers(subscriber_id),  
@@ -131,7 +153,6 @@ CREATE TABLE IF NOT EXISTS stg_roam_in (
     operator_id INT
 );
 
-
 CREATE TABLE IF NOT EXISTS load_operators (
     id           SERIAL PRIMARY KEY,
     tadig        TEXT,
@@ -157,76 +178,7 @@ CREATE TABLE IF NOT EXISTS load_prefixes (
     created_by TEXT DEFAULT 'system'
 );
 
--- =========================
--- LOAD CONFIG
--- =========================
-
-INSERT INTO roam_directions (direction, description) 
-VALUES 
-    ('IN', 'ROAM IN'),
-    ('OUT', 'ROAM OUT');
-
-INSERT INTO metrics_type (name, description) 
-VALUES 
-    ('GLOBAL', 'GLOBAL'),
-    ('COUNTRY', 'COUNTRY'),
-    ('OPERATOR', 'OPERATOR'),
-    ('SUBSCRIBER', 'SUBSCRIBER');
-
-INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description)
-SELECT 
-    rd.roam_direction_id,
-    mt.metric_type_id,
-    'TOT_DAILY_ROUT',
-    'Total daily roamers out'
-FROM roam_directions rd
-CROSS JOIN metrics_type mt
-WHERE rd.direction = 'OUT' AND mt.name = 'GLOBAL';
-
-INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description)
-SELECT 
-    rd.roam_direction_id,
-    mt.metric_type_id,
-    'TOT_DAILY_ROUT_COUNTRY',
-    'Total daily roamers out by country'
-FROM roam_directions rd
-CROSS JOIN metrics_type mt
-WHERE rd.direction = 'OUT' AND mt.name = 'COUNTRY';
-
-INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description)
-SELECT 
-    rd.roam_direction_id,
-    mt.metric_type_id,
-    'TOT_DAILY_ROUT_OPERATOR',
-    'Total daily roamers out by operator'
-FROM roam_directions rd
-CROSS JOIN metrics_type mt
-WHERE rd.direction = 'OUT' AND mt.name = 'OPERATOR';
-
-INSERT INTO periods (
-    date, year, quarter, month, day, day_of_week, day_name,
-    week_of_year, is_weekend, period
-)
-SELECT
-    d::date AS date,
-    EXTRACT(YEAR FROM d) AS year,
-    EXTRACT(QUARTER FROM d) AS quarter,
-    EXTRACT(MONTH FROM d) AS month,
-    EXTRACT(DAY FROM d) AS day,
-    EXTRACT(ISODOW FROM d) AS day_of_week,
-    TO_CHAR(d, 'FMDay') AS day_name,
-    EXTRACT(WEEK FROM d) AS week_of_year,
-    CASE WHEN EXTRACT(ISODOW FROM d) IN (6, 7) THEN TRUE ELSE FALSE END AS is_weekend,
-    TO_CHAR(d, 'YYYY-MM-DD') AS date_text
-FROM GENERATE_SERIES(
-    (DATE_TRUNC('year', NOW()) - INTERVAL '1 year')::DATE,
-    (DATE_TRUNC('year', NOW()) + INTERVAL '5 years - 1 day')::DATE,
-    '1 day'::INTERVAL
-) AS d;    
-
-
-
-COPY countries (iso, name, name_fr, prefix)
+COPY countries (iso,common_name,name_en,name_fr,prefix,prefix_flag)
 FROM '/countries.csv'
 DELIMITER ',' CSV HEADER;
 
@@ -238,8 +190,51 @@ COPY load_prefixes (country, operator, cc, ndc, prefix)
 FROM '/prefixes.csv'
 DELIMITER ',' CSV HEADER;
 
-update countries set created_by = 'system';
 
+INSERT INTO dates (
+    date, year, quarter, month, day, day_of_week, day_name,
+    week_of_year, is_weekend, date_str
+)
+SELECT
+    d::date AS date,
+    EXTRACT(YEAR FROM d) AS year,
+    EXTRACT(QUARTER FROM d) AS quarter,
+    EXTRACT(MONTH FROM d) AS month,
+    EXTRACT(DAY FROM d) AS day,
+    EXTRACT(ISODOW FROM d) AS day_of_week,
+    TO_CHAR(d, 'FMDay') AS day_name,
+    EXTRACT(WEEK FROM d) AS week_of_year,
+    CASE WHEN EXTRACT(ISODOW FROM d) IN (6, 7) THEN TRUE ELSE FALSE END AS is_weekend,
+    TO_CHAR(d, 'YYYY-MM-DD') AS date_str
+FROM GENERATE_SERIES(
+    (DATE_TRUNC('year', NOW()) - INTERVAL '1 year')::DATE,
+    (DATE_TRUNC('year', NOW()) + INTERVAL '5 years - 1 day')::DATE,
+    '1 day'::INTERVAL
+) AS d;    
+
+INSERT INTO gloabl_config (key,value) VALUES ('home_country','Tunisia');
+INSERT INTO gloabl_config (key,value) VALUES ('home_operator','Orange');
+INSERT INTO roam_directions (direction, description) 
+VALUES 
+    ('IN', 'ROAM IN'),
+    ('OUT', 'ROAM OUT');
+INSERT INTO metrics_type (name, description) 
+VALUES 
+    ('GLOBAL', 'GLOBAL'),
+    ('COUNTRY', 'COUNTRY'),
+    ('OPERATOR', 'OPERATOR'),
+    ('SUBSCRIBER', 'SUBSCRIBER');
+UPDATE countries SET created_by = 'system';
+
+INSERT INTO networks (tadig, plmn, mcc, mnc, tech_2g, tech_3g, tech_lte, created_by)
+SELECT tadig, plmn, mcc, mnc, t2g, t3g, lte, created_by
+FROM load_operators;
+
+INSERT INTO operators (operator, brand, country_id, network_id, created_by)
+SELECT ldr.operator, ldr.brand, c.country_id, n.network_id, ldr.created_by
+FROM load_operators ldr
+JOIN countries c ON ldr.country_iso = c.iso
+JOIN networks n ON ldr.tadig = n.tadig;
 
 INSERT INTO prefixes (country_id, cc, prefix, created_by)
 SELECT 
@@ -250,26 +245,13 @@ SELECT
 FROM 
   countries,
   unnest(string_to_array(prefix, ',')) AS prefix_item
-  WHERE iso not in ('FK', 'GP', 'BL', 'MF', 'AX', 'CX', 'CC', 'KZ', 'RU' , 'BN', 'CW', 'BV', 'SJ', 'EH', 'GG', 'IM', 'JE', 'CA', 'US', 'PR', 'TF', 'YT', 'RE','AQ','HM','NF');
-
-
-INSERT INTO networks (tadig, plmn, mcc, mnc, tech_2g, tech_3g, tech_lte, created_by)
-SELECT tadig, plmn, mcc, mnc, t2g, t3g, lte, created_by
-FROM load_operators;
-
-
-INSERT INTO operators (operator, brand, country_id, network_id, created_by)
-SELECT ldr.operator, ldr.brand, c.country_id, n.network_id, ldr.created_by
-FROM load_operators ldr
-JOIN countries c ON ldr.country_iso = c.iso
-JOIN networks n ON ldr.tadig = n.tadig;
-
+  WHERE prefix_flag = 'X';
 
 INSERT INTO prefixes (country_id,operator_id, cc, ndc, prefix, created_by)
 SELECT cnt.country_id, opr.operator_id, ldr.cc, ldr.ndc, ldr.prefix, ldr.created_by
 FROM load_prefixes ldr
 JOIN operators opr ON opr.operator = ldr.operator
-JOIN countries cnt ON cnt.name = ldr.country
+JOIN countries cnt ON cnt.common_name = ldr.country
 WHERE opr.country_id = cnt.country_id;
 
 DELETE FROM prefixes
@@ -277,27 +259,131 @@ WHERE prefix IN (
     SELECT prefix
     FROM (
         SELECT prefix,
-               ROW_NUMBER() OVER (PARTITION BY prefix ORDER BY prefixe_id) AS rn
+               ROW_NUMBER() OVER (PARTITION BY prefix ORDER BY prefix_id) AS rn
         FROM prefixes
     ) t
     WHERE t.rn > 1
 );
 
+DROP TABLE load_operators;
+DROP TABLE load_prefixes;
 
 
--- DROP TABLE load_operators;
--- DROP TABLE load_prefixes;
+----------------------
+-- Business
+----------------------
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_subscribers_in',
+    'Number of subscribers IN',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'IN' AND mt.name = 'GLOBAL';
 
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_subscribers_in_by_country',
+    'Number of subscribers IN (By country)',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'IN' AND mt.name = 'COUNTRY';
 
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_subscribers_in_by_operator',
+    'Number of subscribers IN (By operator)',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'IN' AND mt.name = 'OPERATOR';
 
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_active_subscribers_in',
+    'Number of active subscribers IN',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'IN' AND mt.name = 'GLOBAL';
 
-CREATE TABLE IF NOT EXISTS batch_execs (
-    id SERIAL PRIMARY KEY,
-    batch_name TEXT NOT NULL,
-    source_type TEXT,
-    source_name TEXT,
-    start_time TIMESTAMP,
-    end_time TIMESTAMP,        
-    corr_id INT,
-    batch_status TEXT
-);
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_active_subscribers_in_by_country',
+    'Number of active subscribers IN (By country)',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'IN' AND mt.name = 'COUNTRY';
+
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_active_subscribers_in_by_operator',
+    'Number of active subscribers IN (By operator)',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'IN' AND mt.name = 'OPERATOR';
+
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_subscribers_out',
+    'Number of subscribers OUT',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'OUT' AND mt.name = 'GLOBAL';
+
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_subscribers_out_by_country',
+    'Number of subscribers OUT (By country)',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'OUT' AND mt.name = 'COUNTRY';
+
+INSERT INTO metric_definition (roam_direction_id, metric_type_id, name, description, is_active)
+SELECT 
+    rd.roam_direction_id,
+    mt.metric_type_id,
+    'number_subscribers_out_by_operator',
+    'Number of subscribers OUT (By operator)',
+    TRUE
+FROM roam_directions rd
+CROSS JOIN metrics_type mt
+WHERE rd.direction = 'OUT' AND mt.name = 'OPERATOR';
+
+----------------------
+-- Indexes
+----------------------
+CREATE INDEX idx_metrics_i1 ON metrics (date_id,country_id,operator_id);
+
+----------------------
+-- Views
+----------------------
+create view v_roam_in_metrics as
+select ms.name as metric_type, md.name as metric_name, md.description as metric_description, pr.date_str,cn.common_name as country, op.operator, mt.value  
+from metrics mt 
+join metric_definition md on mt.metric_definition_id = md.metric_definition_id
+join dates pr on pr.date_id = mt.date_id
+join metrics_type ms on ms.metric_type_id = md.metric_type_id
+left join countries cn on cn.country_id = mt.country_id
+left join operators op on op.operator_id = mt.operator_id;
